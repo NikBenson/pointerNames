e2a3241e0dd7e81df6e427276803b74a78ce5f20
DasNik03 <NikBenson03@gmail.com>
2020-06-20 21:55:20 +0200

    init method signatures

--- library.c
+++ library.c
@@ -2,6 +2,26 @@
 
 #include <stdio.h>
 
-void hello(void) {
-	printf("Hello, World!\n");
+struct PointerToNameDictionary* pointerToNameDictionary = NULL;
+struct PointerToNameNames* pointerToNameNames = {"test"};
+
+const char *getName(void* ptr) {
+	if(pointerToNameDictionary == NULL) {
+		pointerToNameDictionary = PointerToNameDictionary_new(ptr, pointerToNameNames->name);
+		pointerToNameNames = pointerToNameNames->next;
+		return pointerToNameDictionary->value;
+	}
+
+	struct PointerToNameDictionary* dict = pointerToNameDictionary;
+
+	if(dict->key == ptr) return dict->value;
+	while(dict->next != NULL) {
+		if(dict->key == ptr) return dict->value;
+		dict = dict->next;
+	}
+
+	dict->next = PointerToNameDictionary_new(ptr, pointerToNameNames->name);
+	pointerToNameNames = pointerToNameNames->next;
+
+	return dict->next->value;
 }
--- library.h
+++ library.h
@@ -1,6 +1,25 @@
 #ifndef POINTERNAMES_LIBRARY_H
 #define POINTERNAMES_LIBRARY_H
 
-void hello(void);
+#include <stdio.h>
+
+struct PointerToNameDictionary *PointerToNameDictionary_new(void* k, const char v[]) {
+	struct PointerToNameDictionary* next = NULL;
+
+	const void* key = k;
+	const char* value = v;
+};
+
+void PointerToNameDictionary_del(struct PointerToNameDictionary* p) {
+
+};
+
+struct PointerToNameNames *PointerToNameNames_new(const char n[]) {
+	struct PointerToNameDictionary* next = NULL;
+
+	const char* name = n;
+};
+
+const char* getName(void* ptr);
 
 #endif //POINTERNAMES_LIBRARY_H
