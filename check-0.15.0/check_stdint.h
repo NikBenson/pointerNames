4a492f999377a60f9ae6d359868566007ffce51d
Nik <NikBenson@pm.me>
2020-06-23 21:05:22 +0200

    undo build test lib

--- check-0.15.0/check_stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CHECK_CHECK_STDINT_H
-#define _CHECK_CHECK_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "check 0.15.0"
-/* generated using gnu compiler Apple clang version 11.0.0 (clang-1100.0.33.12) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
