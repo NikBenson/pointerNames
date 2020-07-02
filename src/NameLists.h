2ebbef73eedc10b31d4f271623304bdbb160e52c
Nik <NikBenson@pm.me>
2020-07-02 17:01:10 +0200

    init NameLists

---
+++ src/NameLists.h
@@ -0,0 +1,26 @@
+#ifndef POINTERNAMES_NAMELISTS_H
+#define POINTERNAMES_NAMELISTS_H
+
+const char fantasyCreatures[][16] = {
+		"Vampire", //TODO
+};
+
+const char starWars[][16] = {
+		"Obiwan", //TODO
+};
+
+const char programmingLanguages[][16] = {
+		"C", //TODO
+};
+
+const char greekMythology[][16] = {
+		"Thor", //TODO
+};
+
+const char videoGames[][16] = {
+		"COD", //TODO
+};
+
+//TODO ...
+
+#endif //POINTERNAMES_NAMELISTS_H
